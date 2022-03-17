f1a4ff1937a308c3c4f85234f25a6f51c1ea6a5c
Sasha Ermohin <saha22023@gmail.com>
2022-03-17 12:33:09 +0700

    Initial commit

---
+++ js/drag.js
@@ -0,0 +1,26 @@
+// Set icons as draggable
+window.onload = function() {
+    draggable(document.getElementById('icon-oneshot'));
+    draggable(document.getElementById('icon-customize'));
+}
+
+// Function for make div (icons) draggable
+function draggable(el) {
+    el.addEventListener('mousedown', function(e) {
+        var offsetX = e.clientX - parseInt(window.getComputedStyle(this).left);
+        var offsetY = e.clientY - parseInt(window.getComputedStyle(this).top);
+
+        function mouseMoveHandler(e) {
+            el.style.top = (e.clientY - offsetY) + 'px';
+            el.style.left = (e.clientX - offsetX) + 'px';
+        }
+
+        function reset() {
+            window.removeEventListener('mousemove', mouseMoveHandler);
+            window.removeEventListener('mouseup', reset);
+        }
+
+        window.addEventListener('mousemove', mouseMoveHandler);
+        window.addEventListener('mouseup', reset);
+    });
+}
