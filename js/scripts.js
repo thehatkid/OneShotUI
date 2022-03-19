6331dd9e9dcbc1e7df44ea639d02d8c8403b9ea9
Sasha Ermohin <saha22023@gmail.com>
2022-03-19 20:44:56 +0700

    Improve taskbar, add elements on taskbar

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
-// Set icons as draggable
 window.onload = function() {
+    // Set icons as draggable
     draggable(document.getElementById('icon-oneshot'));
     draggable(document.getElementById('icon-customize'));
+
+    // Run clock displaying function every 0.5 seconds
+    setInterval(displayTime, 500);
 }
 
 // Function for make div (icons) draggable
@@ -23,4 +26,24 @@ function draggable(el) {
         window.addEventListener('mousemove', mouseMoveHandler);
         window.addEventListener('mouseup', reset);
     });
+}
+
+// Function for displaying clock on Task Bar
+function displayTime() {
+    var clock = document.getElementById('tb-clock');
+    var date = new Date();
+
+    var hours = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
+    var minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+    var am_pm = date.getHours() >= 12 ? "PM" : "AM";
+
+    hours = hours < 10 ? "0" + hours : hours;
+    time = hours + ":" + minutes  + " " + am_pm;
+
+    if (time === clock.innerHTML) {
+        return false;
+    } else {
+        clock.innerHTML = time;
+        return true;
+    }
 }
